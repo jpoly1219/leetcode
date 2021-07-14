4ba4ec6e4c72cbe825331773d1d09ef58a179f45
Jacob Kim <jpoly1219@gmail.com>
2021-07-14 14:00:37 +0000

    solved problem 2

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,4 +1,6 @@
-# define NULL __null
+#include <cmath>
+
+using namespace std;
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -8,47 +10,63 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+void append(struct ListNode** head, int node_data);
+
+int carryOver = 0;
 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum1 = 0;
-        int sum2 = 0;
-        int multiplier = 1;
-        while (l1->next) {
-            sum1 += multiplier * l1->val;
-            multiplier *= 10;
-        }
-        multiplier = 1;
-        while (l2->next) {
-            sum2 += l2->val;
-            multiplier *= 10;
-        }
-
-        int finalSum = sum1 + sum2;
         ListNode* l3 = NULL;
-        while (finalSum >= 1) {
-
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+        carryOver = 0;
+        while (l1 != NULL || l2 != NULL) {
+            if (l1 != NULL) {
+                a = l1->val;
+                l1 = l1->next;
+            }
+            else {
+                a = 0;
+            }
+            if (l2 != NULL) {
+                b = l2->val;
+                l2 = l2->next;
+            }
+            else {
+                b = 0;
+            }
+            sum = a + b + carryOver;
+            if (sum >= 10) {
+                carryOver = floor(sum/10);
+                sum -= carryOver * 10;
+            }
+            else {
+                carryOver = 0;
+            }
+            
+            append(&l3, sum);
         }
-
+        if (carryOver == 1) {
+            append(&l3, 1);
+        }
+        return l3;
     }
 };
 
-// https://www.softwaretestinghelp.com/linked-list/
-
-/*
-2 -> 4 -> 3
-int sum;
-loop through linked list
-    for each element, multiply by 1, 10, 100, ...
-    add them up
-do the above for each linked list
-sum1 + sum2 = sum3
-
-ListNode* l3
-while sum3 > 1 {
-    digit = sum3 % 10
-    l3.append(digit)
-    sum3 = floor(sum3 / 10)
+void append(struct ListNode** head, int node_data) {
+    struct ListNode* newNode = new ListNode;
+    struct ListNode *last = *head;
+    newNode->val = node_data;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = newNode;
+    return;
 }
-*/
