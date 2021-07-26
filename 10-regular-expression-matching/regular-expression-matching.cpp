3df0b481d21d0c9ee55253802e006fffde63291c
Jacob Kim <jpoly1219@gmail.com>
2021-07-26 14:45:12 +0000

    brainstorming problem 10

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -8,3 +8,19 @@ public:
         
     }
 };
+
+/*
+s = aa, p = a
+if (s == p && "* or . doesn't exist at all") {
+    return true;
+}
+
+when * appears, get its index, and look at the character at index-1.
+
+we can have two indices: one for s and the other for p
+iP moves first, and if there is no * or ., iS moves.
+if (* or . exists) {
+    
+}
+
+*/
