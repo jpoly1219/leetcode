6f08491c7d94b31427fac6056ea4d4953f70179f
Jacob Kim <jpoly1219@gmail.com>
2022-08-14 18:59:04 +0900

    solved p10

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,13 +1,186 @@
+#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        // recursion
+
+        // simple case without a star
+        cout << s << " " << p << endl;
+        if (p == "") {
+            return (s.length() == 0);
+        }
+        bool firstMatch = false;
+        if (s != "" && (s.at(0) == p.at(0) || p.at(0) == '.')) {
+            firstMatch = true;
+        }
+
+        if (p.length() >= 2 && p.at(1) == '*') {
+            return (isMatch(s, p.substr(2)) || (firstMatch && isMatch(s.substr(1), p)));
+        } else {
+            return firstMatch && isMatch(s.substr(1), p.substr(1));
+        }
         
+
+        // string::iterator sItr = s.begin();
+        // string::iterator pItr = p.begin();
+        // bool hasStar = false;
+
+        // while (pItr != p.end() && sItr != s.end()) {
+        //     if (*sItr == *pItr) {
+        //         pItr++;
+        //         sItr++;
+        //     } else if (*pItr == '.' && *(next(pItr, 1)) == '*') {
+        //         hasStar = true;
+        //         pItr++;
+        //         sItr++;
+        //     } else if (*(next(pItr, 1)) == '*') {
+        //         hasStar = true;
+        //         pItr++;
+        //     } else if (*pItr == '.') {
+        //         pItr++;
+        //         sItr++;
+        //     } else if (*pItr == '*') {
+        //         hasStar = true;
+        //         char pPattern = *(prev(pItr, 1));
+        //         if (*sItr == pPattern) {
+        //             sItr++;
+        //         } else if (pPattern == '.') {
+        //             *(prev(pItr, 1)) = *sItr;
+        //             sItr++;
+        //         } else {
+        //             pItr += 1;
+        //         }
+        //     } else {
+        //         return false;
+        //     }
+        // }
+
+        // if (p.length() != s.length() && hasStar == false) {
+        //     return false;
+        // }
+
+        // if (sItr == s.end() && pItr != p.end()) {
+        //     if (*pItr == '*' && pItr++ == p.end()) {
+        //         return true;
+        //     }
+        //     // dump the remaining p
+        //     string rem = "";
+        //     while (pItr != p.end()) {
+        //         rem.push_back(*pItr);
+        //         if (rem.length() == 1 && rem == "*") {
+        //             pItr++;
+        //         } 
+        //         if (rem.length() == 2) {
+        //             if (rem.back() != '*') {
+        //                 return false;
+        //             } else {
+        //                 rem = "";
+        //             }
+        //         }
+        //         pItr++;
+        //     }
+        //     if (rem.length()%2 == 1) {
+        //         return false;
+        //     }
+        //     return true;
+        // } else if (sItr == s.end() && pItr == p.end()) {   
+        //     return true;
+        // }
+        // return false;
     }
 };
+/*
+    let's try recursion
+    - we know that every time we run into an asterisk, we need to do the matching.
+    aa a*
+    1: a == a
+    2: * > a == a
+
+    aaab c*a*b
+*/
+/*
+    - iterate over p first.
+    - append to pPattern
+    - stop when *pItr == '*'. pPattern should be sth like ac*
+    - compare 
+*/
+/*
+    abbaabab ab*ab
+
+    cmp(a, b)
+
+    sPtr and pPtr has to move together
+    compare first, return false if not matching.
+
+    before moving, check the next char
+    if pNext == '*'
+    - move sPtr only
+    if pNext == '*' && *sPtr != *pPtr
+    - move pPtr only (twice)
+    if pNext == null || sNext != null
+    - return false
+    if sNext == null || pNext != null
+    - return false
+    else
+    - move both
+*/
+/*
+    have two vectors: one for s and one for p
+    iterate through each string and append to the vectors.
+    if current pChar == '*', pop one from sVec.
+    aa a* = ok
+    aa a*a = ok
+    ab a*b = ok
+    a a* = ok
+    do i try flipping it and iterating from the back? (not for now)
+
+    # Happy Ending!
+    s = "ababaaab", p = "ab*a*b"
+
+    - init
+    sPtr = 0, pPtr = 0. sVec = [], pVec = []
+    - extract things before *
+    sPtr = 0, pPtr = 2. sVec = [], pVec = [a, b, *]
+    - if pVec.end() == '*', keep adding sChar until sVec.len = pVec.len. 
+    sPtr = 1, pPtr = 2. sVec = [a, b], pVec = [a, b, *]
+    - if sVec == pVec, clear sVec. save sPtr as bookmark.
+    sPtr = 1, pPtr = 2. sVec = [], pVec = [a, b, *]
+    - if pVec.end() == '*', keep adding sChar until sVec.len = pVec.len.
+    sPtr = 3, pPtr = 2. sVec = [a, b], pVec = [a, b, *]
+    - if sVec == pVec, clear sVec. save sPtr as bookmark.
+    sPtr = 5, pPtr = 2. sVec = [a, a], pVec = [a, b, *]
+    - if it doesn't match, clear both vectors. move sPtr back to bookmark.
+    sPtr = 3, pPtr = 2. sVec = [], pVec = []
+    - back to step 1. extract until *.
+    sPtr = 3, pPtr = 4. sVec = [], pVec = [a, *]
+    - iterate s
+    sPtr = 4, pPtr = 4. sVec = [a], pVec = [a, *]
+    ...
+    sPtr = 6, pPtr = 4. sVec = [a], pVec = [a, *]
+    - Save sPtr = 6 as bookmark.
+    sPtr = 6, pPtr = 4. sVec = [], pVec = [a, *]
+    sPtr = 7, pPtr = 4. sVec = [b], pVec = [a, *]
+    - back to bookmark. Clear sVec and pVec
+    sPtr = 6, pPtr = 4. sVec = [], pVec = []
+    - iterate until p ends
+    sPtr = 6, pPtr = 5. sVec = [], pVec = [b]
+    - if if pVec.end() != '*', keep adding sChar.
+    sPtr = 7, pPtr = 5. sVec = [b], pVec = [b]
+    - if s is done, return true.
+
+    # Sad Ending!
+
+    s = aaabaabb, p = ab*a*b
+*/
+
+
+
+
 
 /*
 s = aa, p = a
